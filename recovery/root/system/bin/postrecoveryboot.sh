03843b4a4c2e8cb2f964e7517a12a99be7932209
Brock5555 <77067332+Brock5555@users.noreply.github.com>
2021-04-25 15:09:36 +0530

    surya: Fix mtp not working after format data or reboot recovery
    - Also add missing partitions

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -6,14 +6,20 @@
 sleep 1
 mount -w /product > /dev/null 2>&1
 mount -w /vendor > /dev/null 2>&1
-mount -w /system > /dev/null 2>&1
+mount -w /system_ext > /dev/null 2>&1
+mount -w /system_root > /dev/null 2>&1
 
 sleep 1
 umount /product > /dev/null 2>&1
 umount /vendor > /dev/null 2>&1
-umount /system > /dev/null 2>&1
+umount /system_ext > /dev/null 2>&1
+umount /system_root > /dev/null 2>&1
 
 sleep 1
+mkdir /data/media
+mkdir /data/media/0
+mkdir /tmp/install
+mkdir /tmp/install/bin
 
 exit 0
 #
