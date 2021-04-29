b12d28c944c3bb79d65386fee102427f7dbb82eb
Brock5555 <77067332+Brock5555@users.noreply.github.com>
2021-04-29 10:43:24 +0530

    Update postrecoveryboot.sh

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -7,13 +7,13 @@ sleep 1
 mount -w /product > /dev/null 2>&1
 mount -w /vendor > /dev/null 2>&1
 mount -w /system_ext > /dev/null 2>&1
-mount -w /system_root > /dev/null 2>&1
+mount -w /system > /dev/null 2>&1
 
 sleep 1
 umount /product > /dev/null 2>&1
 umount /vendor > /dev/null 2>&1
 umount /system_ext > /dev/null 2>&1
-umount /system_root > /dev/null 2>&1
+umount /system > /dev/null 2>&1
 
 sleep 1
 mkdir /data/media
